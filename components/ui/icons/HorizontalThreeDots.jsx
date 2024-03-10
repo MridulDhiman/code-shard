15b273db2c49d1b072c957c72528960e0ae81300
Mridul Dhiman <dhimanmridul91@gmail.com>
2024-03-10 18:44:57 +0530

    more fixes

--- components/ui/icons/HorizontalThreeDots.jsx
+++ components/ui/icons/HorizontalThreeDots.jsx
@@ -6,7 +6,14 @@ const HorizontalThreeDots = ({className ,onClick}) => {
 <svg
 className={className}
 onClick={onClick}
- fill="#000000" height="200px" width="200px" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path class="cls-1" d="M8,6.5A1.5,1.5,0,1,1,6.5,8,1.5,1.5,0,0,1,8,6.5ZM.5,8A1.5,1.5,0,1,0,2,6.5,1.5,1.5,0,0,0,.5,8Zm12,0A1.5,1.5,0,1,0,14,6.5,1.5,1.5,0,0,0,12.5,8Z"></path> </g></svg>
+ fill="#000000" 
+ height="200px"
+  width="200px" 
+  id="Layer_1" 
+  xmlns="http://www.w3.org/2000/svg" 
+  viewBox="0 0 16 16">
+    <g id="SVGRepo_bgCarrier"
+     strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path class="cls-1" d="M8,6.5A1.5,1.5,0,1,1,6.5,8,1.5,1.5,0,0,1,8,6.5ZM.5,8A1.5,1.5,0,1,0,2,6.5,1.5,1.5,0,0,0,.5,8Zm12,0A1.5,1.5,0,1,0,14,6.5,1.5,1.5,0,0,0,12.5,8Z"></path> </g></svg>
   )
 }
 
