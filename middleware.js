cc751601aa1ba6c77e41f2ebc5137cc01456b2d1
Mridul Dhiman <dhimanmridul91@gmail.com>
2024-10-04 00:10:50 +0530

    [fix] middleware build error

--- middleware.js
+++ middleware.js
@@ -1,47 +1,24 @@
-import { auth } from "./auth";
-import {
-  DEFAULT_LOGIN_REDIRECT,
-  apiAuthPrefix,
-  authRoutes,
-  publicRoutes,
-} from "./routes";
 import { NextResponse } from "next/server";
 
-// all the routes should be protected
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
 
-export default auth((req) => {
-  const isLoggedIn = !!req.auth;
-  const { nextUrl } = req;
-  console.log("ROUTE: ", req.nextUrl.pathname);
-  console.log("IS LOGGED IN: ", isLoggedIn);
-  const urlPath = nextUrl.pathname;
-  const isApiAuthRoute = urlPath.startsWith(apiAuthPrefix);
-  const isPublicRoute = publicRoutes.includes(urlPath);
-  const isAuthRoute = authRoutes.includes(urlPath);
-
-  // let the next auth do it's work
-  if (isApiAuthRoute) {
-    return NextResponse.next();
+export function middleware(request) {
+  if (request.method === "OPTIONS") {
+    return new NextResponse(null, { headers: corsHeaders });
   }
 
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-    }
-    return NextResponse.next();
-  }
+  const response = NextResponse.next();
+  Object.entries(corsHeaders).forEach(([key, value]) => {
+    response.headers.set(key, value);
+  });
 
-  if (!isLoggedIn && !isPublicRoute) {
-    return Response.redirect(new URL("/login", nextUrl));
-  }
-
-  if (isLoggedIn && isPublicRoute) {
-    return Response.redirect(new URL("/your-work", nextUrl));
-  }
-
-  return NextResponse.next();
-});
+  return response;
+}
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: "/api/:path*",
 };
